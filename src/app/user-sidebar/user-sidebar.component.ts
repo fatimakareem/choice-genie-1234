e92f5d37bec934b0b634908522ee89521164a65a
fatimakarim <you@example.com>
2018-05-08 20:13:57 +0500

    gggggggggggggg

--- src/app/user-sidebar/user-sidebar.component.ts
+++ src/app/user-sidebar/user-sidebar.component.ts
@@ -101,29 +101,31 @@ export class UserSidebarComponent implements OnInit, AfterContentInit {
     //  });
 
   }
-  //   fetchitem() {
-  //     // this.route.params.subscribe(params => {
-  //    //   let zip =  this.sg['product_zipcode'];
-  //     let headers = new Headers();
-  //     headers.append('Content-Type', 'application/json')
-  //    this.http.get(Config.api + 'items_perpage/title/asc/' + this.items + '?page=1', { headers: headers })
-  //   //this.http.get(Config.api + 'monthly/' + this.zip_code + '',{ headers: headers })
-  //  // this.http.get(Config.api + 'filter/' + this.zip_code + '',{ headers: headers })
+    fetchitem(items) {
+      // this.route.params.subscribe(params => {
+     //   let zip =  this.sg['product_zipcode'];
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json')
+     this.http.get(Config.api + 'items_perpage/title/asc/' + items , { headers: headers })
+    //this.http.get(Config.api + 'monthly/' + this.zip_code + '',{ headers: headers })
+   // this.http.get(Config.api + 'filter/' + this.zip_code + '',{ headers: headers })
 
-  //  //  this.http.post(Config.api + 'filter/' + this.zip_code + '', {"month": this.months+" Month", "custom":"['2','8']"},{ headers: headers })
-  //       .subscribe(Res => {
-  //         this.sg['products'] = Res.json()['Results'];
-  //         this.data.changeProducts(this.sg['products']);
-  //         this.allItems = this.sg['products'];
-  //         for (let prod of this.sg['products']) {
-  //           // console.log(prod["plan_information"])
-  //           // console.log(prod["price_rate"])
-  //           prod["plan_information"] = prod["plan_information"].split(',,', 3000);
-  //           prod["price_rate"] = prod["price_rate"].split('..', 3000);
-  //         }
-  //      });
+   //  this.http.post(Config.api + 'filter/' + this.zip_code + '', {"month": this.months+" Month", "custom":"['2','8']"},{ headers: headers })
+        .subscribe(Res => {
+          console.log(Res.json()['results'])
+          this.sg['products'] = Res.json()['results'];
+          this.data.changeProducts(this.sg['products']);
+        //  this.allItems = this.sg['products'];
+          console.log(this.sg['products'])
+          for (let prod of this.sg['products']) {
+            // console.log(prod["plan_information"])
+            // console.log(prod["price_rate"])
+            prod["plan_information"] = prod["plan_information"].split(',,', 3000);
+            prod["price_rate"] = prod["price_rate"].split('..', 3000);
+          }
+       });
 
-  //     }
+      }
   months;
   fetchProducts() {
     // this.route.params.subscribe(params => {
